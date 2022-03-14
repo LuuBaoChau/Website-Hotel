9008e169dca851293523cbc01b849c504a5ac62f
LuuBaoChau <baochau1232001@gmail.com>
2022-03-14 20:04:02 +0700

    Update

---
+++ hotel/resources/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(
+  function () {
+    // Scroll
+    $('a').click(function (event) {
+      $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+      }, 700);
+      event.preventDefault();
+      }
+    )
+  }
+)
+
+
+
+
+
+
